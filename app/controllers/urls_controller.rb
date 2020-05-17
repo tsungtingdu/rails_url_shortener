ba786734dcec4ae7f2569cffde6cce5632998923
tsungtingdu <tsungtingdu@gmail.com>
2020-05-17 16:15:22 +0800

    add create short url action and view

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,5 +1,52 @@
 # frozen_string_literal: true
 
 class UrlsController < ApplicationController
-  def index; end
+  def index
+    @url = Url.new
+  end
+
+  def show
+    @url = Url.find_by short_url: params[:id]
+    redirect_to @url.long_url.to_s
+  end
+
+  def create
+    exist_url = Url.find_by long_url: url_params['long_url']
+    if exist_url.nil? && valid_url?(url_params['long_url'])
+      # create new
+      @short_url = url_generator
+      @result = Url.new(long_url: url_params['long_url'], short_url: @short_url)
+
+      @result = nil unless @result.save
+    elsif exist_url.nil?
+      # user existed
+      @result = exist_url
+    end
+    # back to index page
+    @url = Url.new
+    render :index
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:long_url)
+  end
+
+  def url_generator
+    is_unique = false
+    until is_unique
+      short_url = (0...6).map { rand(65..90).chr }.join.downcase
+      exist_url = Url.find_by short_url: short_url
+      is_unique = true if exist_url.nil?
+    end
+    short_url
+  end
+
+  def valid_url?(url)
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Url < ApplicationRecord
+  validates_presence_of :long_url
 end
