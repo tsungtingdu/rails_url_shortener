45dd234c7b554489562a56506408db7d793e8c2f
tsungtingdu <tsungtingdu@gmail.com>
2020-05-17 13:55:54 +0800

    create urls routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :urls, only: %i[index show create]
 end
